494b01754e79fdc956f759676ef1900ef3069f12
rammafitraCYT <rammafitracyt@gmail.com>
2024-06-20 18:24:38 +0700

    done

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ffmpeg -i rtsp://admin:delameta1407@192.168.150.190:554/main -c:v libx264 -hls_time 10 -hls_list_size 0 -f hls /home/delameta/rananum/rtsp/playlist.m3u8
+ffmpeg -i rtsp://xxxxx:xxxxxxxxxxxx@xxx.xxx.xxx.xxx:xxx/main -c:v libx264 -hls_time 10 -hls_list_size 0 -f hls /home/delameta/rananum/rtsp/playlist.m3u8
 
